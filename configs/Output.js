e3bed0926e133ed8a51837022209bd1d91f61f42
Luke Policinski <luke@lukepolo.com>
2018-08-07 18:38:22 -0400

    set the public path to be the root

--- configs/Output.js
+++ configs/Output.js
@@ -2,6 +2,7 @@ const Config = require("./Config");
 module.exports = class Output extends Config {
   register() {
     this.webpackChain.output
+      .publicPath('/')
       .path(this.config.outputPath)
       .filename(`js/[name]-[${this.config.hashType}].js`)
       .chunkFilename(`js/[name]-[${this.config.hashType}].js`);
