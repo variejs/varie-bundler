bc59eefe915c619c1ff60e5f2d7c5b3d61762a77
Luke Policinski <luke@lukepolo.com>
2019-03-21 08:15:55 -0400

    Removing basic

--- plugins/WebpackBar.js
+++ plugins/WebpackBar.js
@@ -5,7 +5,6 @@ module.exports = class WebpackBar extends Plugin {
   register() {
     this.webpackChain.plugin("webpackbar").use(Webpackbar, [
       {
-        basic: false,
         name: this.options.name || "Client",
       },
     ]);
